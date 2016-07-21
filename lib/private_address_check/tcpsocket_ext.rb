043e90cad6528f901efd160d29d81bf54e4bbda7
John Downey <jdowney@gmail.com>
2016-07-21 08:19:33 -0500

    Namespace TCP socket extension
    This will prevent an weird naming conflicts and also make the ancestors
    chain much more clear.

--- lib/private_address_check/tcpsocket_ext.rb
+++ lib/private_address_check/tcpsocket_ext.rb
@@ -9,21 +9,21 @@ module PrivateAddressCheck
   ensure
     Thread.current[:private_address_check] = false
   end
-end
 
-module TCPSocketExt
-  def initialize(remote_host, remote_port, local_host = nil, local_port = nil)
-    if Thread.current[:private_address_check]
-      ip = Resolv.getaddress(remote_host)
-      if PrivateAddressCheck.private_address?(ip)
-        raise PrivateAddressCheck::PrivateConnectionAttemptedError
+  module TCPSocketExt
+    def initialize(remote_host, remote_port, local_host = nil, local_port = nil)
+      if Thread.current[:private_address_check]
+        ip = Resolv.getaddress(remote_host)
+        if PrivateAddressCheck.private_address?(ip)
+          raise PrivateAddressCheck::PrivateConnectionAttemptedError
+        end
+
+        super(ip, remote_port, local_host, local_port)
+      else
+        super
       end
-
-      super(ip, remote_port, local_host, local_port)
-    else
-      super
     end
   end
 end
 
-TCPSocket.prepend(TCPSocketExt)
+TCPSocket.prepend(PrivateAddressCheck::TCPSocketExt)
