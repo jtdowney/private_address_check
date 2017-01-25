aa8fcb4d999f1fcd4c92a92025360783ea435b21
John Downey <jdowney@gmail.com>
2017-01-25 08:04:48 -0600

    Use class_eval to extend TCPSocket instead of prepend
    While prepend is cleaner, it was leading to infinite recursion if someone else
    overrode the initialize method. This happens in resolv-replace in the ruby
    stdlib and happens to be used by the newrelic resque agent.

--- lib/private_address_check/tcpsocket_ext.rb
+++ lib/private_address_check/tcpsocket_ext.rb
@@ -9,16 +9,16 @@ module PrivateAddressCheck
   ensure
     Thread.current[:private_address_check] = false
   end
-
-  module TCPSocketExt
-    def initialize(remote_host, remote_port, local_host = nil, local_port = nil)
-      if Thread.current[:private_address_check] && PrivateAddressCheck.resolves_to_private_address?(remote_host)
-        raise PrivateAddressCheck::PrivateConnectionAttemptedError
-      end
-
-      super
-    end
-  end
 end
 
-TCPSocket.send(:prepend, PrivateAddressCheck::TCPSocketExt)
+TCPSocket.class_eval do
+  alias initialize_without_private_address_check initialize
+
+  def initialize(remote_host, remote_port, local_host = nil, local_port = nil)
+    if Thread.current[:private_address_check] && PrivateAddressCheck.resolves_to_private_address?(remote_host)
+      raise PrivateAddressCheck::PrivateConnectionAttemptedError
+    end
+
+    initialize_without_private_address_check(remote_host, remote_port, local_host, local_port)
+  end
+end
